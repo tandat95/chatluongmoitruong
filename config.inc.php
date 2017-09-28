9bd074d28b8b4cb27c19f32e452498f4386582b8
tandat95 <32350998+tandat95@users.noreply.github.com>
2017-09-28 21:31:29 +0700

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,3 +1,6 @@
 <?php 
- $link = pg_connect('host=localhost dbname=postgres user=postgres password=tandat123');
+ $link = pg_connect('host=ec2-107-22-187-21.compute-1.amazonaws.com
+ dbname=dcgrk1jqrqn8p5 
+ user=tsvvfuztuuqmcd
+ password=430d55c847752133dc3a9faba2e38157bcbf8873fae1d2f2f2df7c749fc85539s');
 ?>
